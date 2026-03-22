bea6eaad5502b01514776cd1305ff2175a852f0c
Nickolas <nickolasalmeida2@gmail.com>
2026-03-22 17:13:33 -0300

    chore: updates rules for suport assets .jpg .png etc

--- webpack.rules.js
+++ webpack.rules.js
@@ -26,6 +26,10 @@ module.exports = [
       }
     }
   },
+  {
+    test: /\.(png|jpe?g|gif|svg)$/i,
+    type: 'asset/resource',
+  }
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
