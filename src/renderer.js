46c9d391865c30260771e0b6da663e55e849385f
Nickolas <nickolasalmeida2@gmail.com>
2026-04-23 09:19:42 -0300

    feat: turn home2 the initial screen

--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './View/App'; // 👇 Importamos o App, não o MenuNav
 import './View/index.css'; 
 import PlayerView from './View/PlayerView/PlayerView';
+import App from './View/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> 
+    <App/>
   </React.StrictMode>
 );
