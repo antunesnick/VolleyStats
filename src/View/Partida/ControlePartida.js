98e5b425747e4abaabb3413e03b9d6665b0ee1d6
Nickolas <nickolasalmeida2@gmail.com>
2026-04-28 11:16:28 -0300

    feat: shows name of gymnasium in controle partica screen

--- src/View/Partida/ControlePartida.js
+++ src/View/Partida/ControlePartida.js
@@ -268,7 +268,7 @@
       return {
         name: partida?.nome || 'Controle de Partida',
         date: matchDate,
-        gymnasium: "Arena Central"
+        gymnasium: partida?.ginasioNome || "Arena Central"
       };
     }, [partida]);
 
